4f5f672e76cb85a58dca1a1937c9da5c995a2679
christoffer <christoffer.g.persson@gmail.com>
2023-04-12 21:38:36 +0200

    refactor backend to separate directory

--- backend/api.py
+++ backend/api.py
@@ -16,16 +16,16 @@ CLEANING_INTERVAL = 0.1
 
 @app.get("/", response_class=HTMLResponse)
 async def index():
-    f = open(f'frontend/index.html', "r")
+    f = open(f'../frontend/index.html', "r")
     return f.read()
 
-@app.get("api/profile/{profile_id}")
+@app.get("/api/profile/{profile_id}")
 async def read_results(profile_id: str):
     profile_result = profile_store.get_profile(profile_id)
     profile_result['timestamp'] = profile_result['timestamp'].isoformat()
     return Response(content=json.dumps(profile_result))
 
-@app.post("api/profile/")
+@app.post("/api/profile")
 async def profile_file(files: List[UploadFile]):
     profile_id = await profile_store.create_profile(files[0])
     response = {'profile_id': profile_id}
